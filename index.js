b0cf7f341ac2ae4b3b3d961395f77d17483bb80f
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-17 12:13:28 -0500

    pass recipe-form-template specs

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  addFormTemplate();
 }
+
+function addFormTemplate() {
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  let fn = Handlebars.compile(template);
+  let html = fn({ ingredients: Array(5).fill({ name: "" })});
+  document.querySelector("main").innerHTML += html;
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
+  init();
+});
