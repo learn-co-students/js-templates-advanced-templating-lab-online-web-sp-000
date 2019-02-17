863e512e9077eefc88cc4ca74e1b6b4ef69efba0
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-17 13:11:45 -0500

    display recipe

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 function init() {
+  Handlebars.registerPartial(
+    "recipeDetailsPartial",
+    document.getElementById("recipe-details-partial").innerHTML
+  );
+  Handlebars.registerPartial(
+    "ingredientsPartial",
+    document.getElementById("ingredients-partial").innerHTML
+  );
+
+  Handlebars.registerHelper('ingredient_body', function(){
+    return new Handlebars.SafeString(`<li>${this.name}</li>`)
+  })
   addFormTemplate();
+  addRecipeTemplate();
+  addDetailsPartial();
 }
 
 function addFormTemplate() {
   let template = document.getElementById("recipe-form-template").innerHTML;
   let fn = Handlebars.compile(template);
-  let html = fn({ ingredients: Array(5).fill({ name: "" })});
+  let html = fn({ ingredients: Array(5).fill({ name: "" }) });
   document.querySelector("main").innerHTML += html;
 }
 
+function handleSubmit() {
+  let name = document.getElementById("name").value;
+  let description = document.getElementById("description").value;
+  let ingredients = [];
+  let form = document.getElementById("recipe-form");
+  let ingredientFields = form.querySelectorAll("input[name=ingredients]");
+  ingredientFields.forEach(i => {
+    ingredients.push({ name: i.value });
+  });
+  recipe = { name: name, description: description, ingredients: ingredients };
+}
+
+let recipe;
+
+function addRecipeTemplate() {
+  let template = document.getElementById("recipe-template").innerHTML;
+  let fn = Handlebars.compile(template);
+  let html = fn(recipe);
+  document.querySelector("main").innerHTML += html;
+}
+
+function addDetailsPartial() {}
+
+function displayEditForm() {}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init();
+  mocha.run();
 });
