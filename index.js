0f13049c13a91ca5d45e62ddacd6acea15062189
Jonathan Tuzman <tuzmusic@gmail.com>
2019-02-18 16:52:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function init() {
-  addHelpers()
+  addHelpers();
   addPartials();
   addFormTemplate();
   // addRecipeTemplate();
 }
 
-function addRecipeTemplate() {
-
-}
+function addRecipeTemplate() {}
 
 function addFormTemplate() {
   let template = document.getElementById("recipe-form-template").innerHTML;
@@ -17,11 +15,32 @@ function addFormTemplate() {
 }
 
 function handleSubmit() {
-  let recipe = recipeFromForm()
+  let recipe = recipeFromForm();
   let template = document.getElementById("recipe-template").innerHTML;
   let fn = Handlebars.compile(template);
-  let html = fn(recipe)
+  let html = fn(recipe);
   document.querySelector("main").innerHTML = html;
+}
+
+function displayEditForm() {
+  let recipe = recipeFromTemplate()
+  let template = document.getElementById("recipe-form-template").innerHTML;
+  let fn = Handlebars.compile(template);
+  let html = fn(recipe);
+  document.querySelector("main").innerHTML = html;
+}
+
+function recipeFromTemplate() {
+  let name = document.getElementById("recipeName").innerHTML;
+  let description = document.getElementById("recipeDescription").innerHTML;
+
+  let ingredients = [];
+  let ingredientFields = document.querySelectorAll("[name=ingredients]");
+  ingredientFields.forEach(i => {
+    ingredients.push(i.innerHTML);
+  });
+
+  return { name: name, description: description, ingredients: ingredients };
 }
 
 function recipeFromForm() {
@@ -32,13 +51,11 @@ function recipeFromForm() {
   let form = document.getElementById("recipe-form");
   let ingredientFields = form.querySelectorAll("input[name=ingredients]");
   ingredientFields.forEach(i => {
-    if (i.value.length > 0) 
-      ingredients.push(i.value);
+    if (i.value.length > 0) ingredients.push(i.value);
   });
 
   return { name: name, description: description, ingredients: ingredients };
 }
-
 
 function addPartials() {
   Handlebars.registerPartial(
@@ -52,12 +69,12 @@ function addPartials() {
 }
 
 function addHelpers() {
-  Handlebars.registerHelper("displayIngredient", function (ingredient) {
-    return new Handlebars.SafeString(`<li name="ingredients">${ingredient}</li>`);
+  Handlebars.registerHelper("displayIngredient", function(ingredient) {
+    return new Handlebars.SafeString(
+      `<li name="ingredients">${ingredient}</li>`
+    );
   });
 }
-
-function displayEditForm() {}
 
 document.addEventListener("DOMContentLoaded", function(event) {
   init();
